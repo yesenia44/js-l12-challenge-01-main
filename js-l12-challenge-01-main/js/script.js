b05a570b5a52c251aae75bac620af1ca40b2829a
yesenia_v <yeseniavsqz@gmail.com>
2024-10-01 13:24:55 -0700

    Created new functions and an event listener to display a random image when someone clicks the button

--- js-l12-challenge-01-main/js/script.js
+++ js-l12-challenge-01-main/js/script.js
@@ -3,10 +3,31 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-getImage = async function () {
+const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images);
 
 };
-getImage()
+
+
+
+const selectRandomImage = function(images){
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+    const randomImage = images[randomIndex]
+    //console.log(randomImage);
+    displayImage(randomImage);
+}
+const displayImage = function(randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+button.addEventListener("click", function() {
+    getImage()
+
+})
